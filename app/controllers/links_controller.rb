0c7626c6d73590270df686145e92239c427e9e9d
Laura <llauranguyen@gmail.com>
2014-11-06 15:13:05 -0800

    created input field and return table partial

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,31 @@
+class LinksController < ApplicationController
+
+	def index
+		 @link = Link.all
+	    render :partial => 'index'
+	end
+
+	def create
+	    @link = Link.new(task_params)
+	    if @link.save
+	      render 
+	    else
+	      render
+	    end
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+ private
+    def task_params
+      params.require(:link).permit(:link_id, :comments)
+ end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 root 'reddits#index'
-resources :reddits
+resources :reddits, :links
 get 'reddits/:id' => 'reddits#scrap'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
