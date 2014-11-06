44bb5867934a732d426e7af953faa7e93939fc3f
Neil <macadamiannut@gmail.com>
2014-11-06 11:36:40 -0800

    got scrap to work and return json, need to add conditions

--- app/controllers/reddits_controller.rb
+++ app/controllers/reddits_controller.rb
@@ -1,10 +1,20 @@
 class RedditsController < ApplicationController
-
+	
 	def index
 		@link = Link.new
 	end
 
 	def create
+	end
+
+	def scrap	
+	end
+
+	def show
+	link = params[:link] + ".json"
+	@response = HTTParty.get(URI.encode(link))
+    @result = JSON.parse(@response.body)
+    render json: @result
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 root 'reddits#index'
 resources :reddits
+get 'reddits/:id' => 'reddits#scrap'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
