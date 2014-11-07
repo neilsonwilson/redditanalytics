3c24c02e39615cf95656e6459b6c4728f8ca96c3
Neil <macadamiannut@gmail.com>
2014-11-06 20:08:32 -0800

    moved the code to create

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,12 +6,23 @@ class LinksController < ApplicationController
 	end
 
 	def create
-	    @link = Link.new(task_params)
-	    if @link.save
-	      render 
-	    else
-	      render
+		common = ['person', '1', '2', 'i','love']
+
+		@comments = []
+		link = params[:link].to_s + ".json"
+		@response = HTTParty.get(URI.encode(link))
+	    @result = JSON.parse(@response.body)
+	    @result[1]["data"]["children"].each do |comment|
+	    	@comments.push({:content => comment["data"]["body"] , :children => []})
 	    end
+	    words = {}
+	    @comments.each do |comment|
+	    	next unless comment[:content] != nil
+	    	comment[:content].strip.downcase.split(/[^\w']+/).group_by(&:to_s).map{ |w| 
+	    		words[w[0]] = w[1].count unless common.include? w[0]
+	    	} 
+	    end
+		render json: words.to_json
 	end
 
 	def update
